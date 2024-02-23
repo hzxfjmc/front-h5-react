9a112185c2aa0639d8b476958df46a7572ae8ae9
azu <joyhuang@yeahka.com>
2024-02-23 16:39:16 +0800

    feat：组件之间的传值

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,7 +1,33 @@
 import './index.less'
+import React,{ Component } from 'react';
+function Header(props){
+  function handlerClick(){
+    console.log(123213)
+  }
+  return (
+      <div className='header'>
+        <div className="tab" onClick={handlerClick}>{props.title}</div>
+      </div>
+    )
+}
 
-function Index(){
-  return <div>这是Index</div>
+
+class Index extends Component{
+  state = {login: '2133'};
+   
+  handleClick = (e)=> {
+    this.setState({
+      login:'你好'
+    })
+  }
+  render(){
+    return (
+      <div className="block-wrap">
+        <Header title={this.state.login} ></Header>
+        <div className="block-da" onClick={this.handleClick}>3213</div>
+      </div>
+    )
+  }
 }
 
 export default Index
